65a852980e469345326dd9280ab17888ca83c27e
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-09-09 14:52:48 +0800

    Add files via upload

---
+++ OSITA_JOHN_MATTHEW_LA#1.py
@@ -0,0 +1,14 @@
+#la#1
+class hero():
+    name = "Yu Zhong"
+    role = "fighter"
+    hp = 100
+    basic_attack = 300
+    
+fighter = hero() # object
+
+print(fighter.name)
+print(fighter.role)
+print(fighter.hp)
+print(fighter.basic_attack)
+
