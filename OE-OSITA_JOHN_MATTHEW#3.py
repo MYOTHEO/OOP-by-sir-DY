a2f15016d5dd8895a521bd87e6a9328c324d510b
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-09-23 12:59:25 +0800

    OE-OSITA_JOHN_MATTHEW#3.py

--- OE-OSITA_JOHN_MATTHEW#3.py
+++ OE-OSITA_JOHN_MATTHEW#3.py
@@ -18,9 +18,12 @@ class InstagramAccount(SocialMediaAccount):
 class TwitterAccount(SocialMediaAccount):
     def __init__(self, username, password, num_of_tweets):
         super().__init__(username, password)
-        self.num_of_followers = num_of_tweets
+        self.num_of_tweets = num_of_tweets
     def tweet(self):
         print(f"{self.username} has {self.password} and {self.num_of_tweets}")
 
 insta = InstagramAccount("Miya","123","51 followers")
 xbird = TwitterAccount("Miya","123","51 tweets")
+
+insta.share_story()
+xbird.tweet()
