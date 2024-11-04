4ca1110cbd5d4eb6d522ee6e6c5c2714c9d2e235
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-11-04 16:23:20 +0800

    Add files via upload

---
+++ OSITA_JOHN_MATTHEW_LA#25.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    @property #abstract property
+    @abstractmethod
+    def alias(self):
+        pass
+
+class Batman(Character):
+    def __init__(self, real_name, alias):
+        self.real_name = real_name
+        self.__alias = alias
+        
+    @property #abstract property
+    def alias(self):
+        return f"My name is {self.__alias} and I'm the Batman!"
+
+paniki = Batman("Bruce Wayne", "Batman")
+print(paniki.alias)
+
+'''
+print("Hello World"()) # This an Error
+'''
